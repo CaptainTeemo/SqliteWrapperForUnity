8ee5bae468dc2f9fa02a992bf789125a57638c81
ekat19 <chriswoody05@gmail.com>
2014-12-23 17:18:18 +0800

    AppController

---
+++ Sqlite For Unity with CSharp/AppController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppController : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		Application.targetFrameRate = 60;
+		yield return StartCoroutine(DBManager.sharedManager().copyFileToDevice("PersistentData.db"));
+		yield return StartCoroutine(DBManager.sharedManager().copyFileToDevice("UserData.db"));
+		Application.LoadLevel("UI");
+	}
+}
